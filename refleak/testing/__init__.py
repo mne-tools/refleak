3a04631a6e658c445bbcdef3ba6e802a3737b17f
Eric Larson <larson.eric.d@gmail.com>
2026-07-10 08:41:18 -0400

    Add Snapshot class for before-after tests (#6)

--- refleak/testing/__init__.py
+++ refleak/testing/__init__.py
@@ -5,12 +5,14 @@
 # License: BSD-3-Clause
 
 from ._core import (
+    Snapshot,
     assert_no_instances,
     gc_collect_once,
     referrer_chain,
 )
 
 __all__ = [
+    "Snapshot",
     "assert_no_instances",
     "gc_collect_once",
     "referrer_chain",
